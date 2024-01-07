c2428780f305d8f28f8a81ccf880918d6f2665fe
dcw <-->
2024-01-06 21:20:50 -0500

    Reorg

---
+++ aocp_v1/ch_1_3/p16/aocp_v1_ch1.3.1_p16.scm
@@ -0,0 +1,16 @@
+#! /usr/bin/mixguile \
+-e main -s
+!#
+
+(define main
+  (lambda (args)
+    ;; load the file provided as a command line argument
+    (mix-load "aocp_v1_ch1.3.1_p16")
+    ;; execute it
+    (mix-run)
+    ;; get some memory locations
+    (mix-pmem "0-7")
+    (mix-pmem "999-1002")
+    (mix-pmem "2999-3009")
+    ;; print the contents of registers
+    (mix-pall)))
