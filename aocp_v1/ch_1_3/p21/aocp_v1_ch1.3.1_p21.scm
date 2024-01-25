80b3e06b0512dc59febaf5e9f507b889189d5dba
dcw <-->
2024-01-24 22:29:55 -0500

    Made some interesing progress with checker for p21

--- aocp_v1/ch_1_3/p21/aocp_v1_ch1.3.1_p21.scm
+++ aocp_v1/ch_1_3/p21/aocp_v1_ch1.3.1_p21.scm
@@ -2,9 +2,32 @@
 -e main -s
 !#
 
+(define (ct s e) (if (> e s) (cons e (ct s (- e 1))) `(,s)))
+
+(define (listify-mem s e)
+  (define idxs (ct s e))
+  (map mix-cell idxs))
+
+(define (validate-memory expected starting)
+  (cond ((= starting 4000) #t)
+	((not (= (list-ref expected 0) (mix-cell starting)))
+	 (display "failed at: ")
+	 (display starting)
+	 (display " expecting ")
+	 (display (list-ref expected 0))
+	 (display " found ")
+	 (display (mix-cell starting))
+	 #f)
+	(else (validate-memory (cdr expected) (+ starting 1)))))
+
 (define main
   (lambda (args)
     (mix-load "aocp_v1_ch1.3.1_p21")
     (mix-sreg "I4" 123)
-    (mix-run)
+    (define before-mem (listify-mem 0 3999))
+    ;(mix-run)
+    ;(mix-set-cell! 10 101)
+    (if (validate-memory before-mem 0)
+	(display "success\n")
+	(display "failure\n"))
     (mix-pall)))
--- refs/refs.md
+++ refs/refs.md
@@ -3,10 +3,12 @@
 ### Nicematrix docs
 https://us.mirrors.cicku.me/ctan/macros/latex/contrib/nicematrix/nicematrix.pdf
 
-# Scheme
+# Scheme & Lisp
 ## Reference
 Some useful io stuff: 
 https://en.wikibooks.org/wiki/Scheme_Programming/Input_and_Output
+Emacs Lisp:
+https://www.youtube.com/watch?v=RQK_DaaX34Q
 
 # Guile
 ## Reference
