f161e2f068fad1b85dc672f8492889c412c9f7fa
dcw <-->
2024-01-28 20:24:04 -0500

    First try at a lookup table solution

--- aocp_v1/ch_1_3/p22/pow13.scm
+++ aocp_v1/ch_1_3/p22/pow13.scm
@@ -2,10 +2,19 @@
 -e main -s
 !#
 
+(define (ct-nonzero-lines s e)
+  (cond ((> s e) 0)
+	(define subct (ct-nonzero-lines (+ 1 s) e))
+	((= (mix-cell s) 0) subct)
+	(else (+ 1 subct))))
+
 (define main
   (lambda (args)
     (mix-compile "pow13")
     (mix-load "pow13")
+    (display "program instruction count: ")
+    (display (ct-nonzero-lines 0 3999))
+    (display "\n")
     (mix-smem 2000 3)
     (mix-run)
     (mix-pall)))
