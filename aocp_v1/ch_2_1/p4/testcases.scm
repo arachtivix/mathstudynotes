47544098371b8dd0686ca56b5a1fc7c23a272f42
dcw <-->
2024-03-16 20:23:20 -0400

    Test harness more or less in place

---
+++ aocp_v1/ch_2_1/p4/testcases.scm
@@ -0,0 +1,62 @@
+#! /usr/bin/mixguile \
+-e main -s
+!#
+
+(define face-up 0)
+(define face-down 1)
+
+(define clubs 1)
+(define diamonds 2)
+(define hearts 3)
+(define spades 4)
+
+(define ace 1)
+(define jack 11)
+(define queen 12)
+(define king 13)
+
+(define TOP 100)
+(define LAMBDA 0) ; the aocp uppercase lambda/list end marker
+
+(define (card-numerical tag suit rank next)
+  (+ (ash tag 24)
+     (ash suit 18)
+     (ash rank 12)
+     next))
+
+(define (place-card tag suit rank next loc)
+  (define cell-val (card-numerical tag suit rank next))
+  (mix-smem loc cell-val))
+
+(define (run-test scenario program)
+  (display "Starting test")
+  (mix-compile program)
+  (mix-load program)
+  (define expected (scenario))
+  (mix-run)
+  (expected))
+
+(define (scen1)
+  (place-card face-up spades 2 101 100) ; top card
+  (place-card face-down spades 3 102 101)
+  (place-card face-down spades 4 103 102)
+  (place-card face-down hearts queen 104 103)
+  (place-card face-down diamonds jack LAMBDA 104)
+  ; NEWCARD value set at next position but not linked up yet
+  (place-card face-up clubs king LAMBDA 105)
+  (mix-smem 1000 106)
+  (mix-smem 1001 TOP) ; TOP card link variable
+  (lambda ()
+    (mix-pmem 100 108)
+    (display "Expected ")
+    (newline)
+    (display (card-numerical face-down diamonds jack 105))
+    (newline)
+    (display (card-numerical face-up clubs king LAMBDA))
+    (newline)
+    (display "in memory locations 104 and 105 respectively\n")))
+
+(define main
+  (lambda (args)
+    (run-test scen1 "solution")
+    ))
