c3a76319cd3b549f59e7a2a6ecafbb8c082212d7
dcw <-->
2024-03-24 11:47:31 -0400

    Wrapping up problem 4

---
+++ templates/scenarios.scm
@@ -0,0 +1,5 @@
+
+(define (scen1)
+  (display "no scenario defined yet\n")
+  (lambda ()
+    (display "no assertions defined yet\n")))
--- templates/testcases.scm
+++ templates/testcases.scm
@@ -2,18 +2,22 @@
 -e main -s
 !#
 
+(load "scenarios.scm")
+
 (define (ct-nonzero-lines s e)
   (define subct (cond ((> s e) 0)
 		       (else (ct-nonzero-lines (+ 1 s) e))))
   (cond ((or (> s e) (= (mix-cell s) 0)) subct)
 	(else (+ 1 subct))))
 
-(define program "solution")
+(define (run-test scenario program)
+  (mix-compile program)
+  (mix-load program)
+  (define expected (scenario))
+  (display "Starting test\n")
+  (mix-run)
+  (expected))
+
 (define main
   (lambda (args)
-    (mix-compile program)
-    (display "instruction count: ")
-    (display (ct-nonzero-lines 0 3999))
-    (display "\n")
-    (mix-load program)
-    (mix-run)))
+    (run-test scen1 "solution")))
