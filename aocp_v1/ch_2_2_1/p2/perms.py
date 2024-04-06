f41669c8ca1c2ca41a8507c01176f5e85b783230
dcw <-->
2024-04-06 13:55:40 -0400

    Finished with CH2.2.1#2

--- aocp_v1/ch_2_2_1/p2/perms.py
+++ aocp_v1/ch_2_2_1/p2/perms.py
@@ -1,25 +1,23 @@
 #! /usr/bin/python3
 
-from itertools import permutations
-
-#for p in permutations([1,2,3,4]):
-#    print(p)
-
 def sim(inp, stk, out):
     # option 0: we're done already
     if len(inp) == 0 and len(stk) == 0:
-        print(out)
-    
+        lz = ' '.join(out)
+        print(f"{lz}")
+        return
+
+    opts = []
     # option 1: take from input, place in stack
     if len(inp) > 0:
         stk.append(inp.pop(0))
         sim(inp, stk, out)
-        inp.insert(0, stk.pop())
+        inp.insert(0,stk.pop())
 
     # option 2: take from stack, place in output
     if len(stk) > 0:
-        out.insert(0, stk.pop())
+        out.append(stk.pop())
         sim(inp, stk, out)
-        stk.append(out.pop(0))
-
-sim(['1','2','3','4'], [], [])
+        stk.append(out.pop(-1))
+        
+sim(['1','2','3','4','5', '6'], [], [])
