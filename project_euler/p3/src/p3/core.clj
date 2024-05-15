b03b57d940268a5cd1e1cce4d211a439ba36d4c1
dcw <-->
2024-05-14 22:17:19 -0400

    working

--- project_euler/p3/src/p3/core.clj
+++ project_euler/p3/src/p3/core.clj
@@ -8,16 +8,21 @@
 (defn up-by-twos
   [start] (lazy-seq (cons start (up-by-twos (+ start 2)))))
 
-(defn get-factors
-  [n [first-factor & remain :as try-factors]]
-  (cond (= 1 n) '()
-        (= 0 (count try-factors)) (list n)
-        (= 0 (mod n first-factor)) (let [nn (/ n first-factor)]
-                                     (conj (get-factors nn try-factors) first-factor))
-        :else (recur n remain)))
-
 (defn get-check-range
   [n]
   (conj
    (take-while #(<= (* % %) n) (up-by-twos 3))
    2))
+
+(defn get-factors
+  ([n] (get-factors n (get-check-range n)))
+  ([n [first-factor & remain :as try-factors]]
+   (cond (= 1 n)
+         '()
+         (= 0 (count try-factors))
+         (list n)
+         (= 0 (mod n first-factor))
+         (let [nn (/ n first-factor)]
+           (conj (get-factors nn try-factors) first-factor))
+        :else (recur n remain))))
+
