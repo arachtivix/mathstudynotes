9c414e3fc54a9fcf99bc2a30c838114dd567cbdd
dcw <-->
2024-05-27 18:33:43 -0400

    More progress on p361

--- project_euler/p361/src/p361/core.clj
+++ project_euler/p361/src/p361/core.clj
@@ -1,3 +1,18 @@
 (ns p361.core)
 
-(def t
+(defn f
+  ([n]
+   (f n 0))
+  ([n c]
+   (cond (= n 0) c
+         (= n 1) (+ c 1)
+         :else (let [r (mod n 2)
+                     d (bigint (/ n 2))]
+                 (if (= r 0)
+                   (recur d c)
+                   (recur d (+ 1 c)))))))
+
+(def sample-f
+  (map
+   f
+   (range 100)))
