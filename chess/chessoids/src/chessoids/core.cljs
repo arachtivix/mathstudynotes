6b5582cab625f8f16d687d1f792f29d22a7e8919
dcw <-->
2024-10-06 22:25:51 -0400

    Board

--- chess/chessoids/src/chessoids/core.cljs
+++ chess/chessoids/src/chessoids/core.cljs
@@ -20,13 +20,31 @@
     bl {k "♚" q "♛" r "♜" b "♝" n "♞" p "♟"}
 })
 
+(def canvas (.getElementById js/document "spooshl"))
+(def ctx (.getContext canvas "2d"))
+
 ; (piece color rank)
 (defn pc [c r] ((pieces c) r))
 
-(defn draw-pc [c r px]
-    (def canvas (.getElementById js/document "spooshl"))
-    (def ctx (.getContext canvas "2d"))
+(defn draw-pc [c r x y px]
+    (set! (.-fillStyle ctx) "rgb(0,0,255)")
+    (.fillRect ctx x, y, px, px)
     (set! (.-fillStyle ctx) "rgb(255,0,0)")
-    (set! (.-font ctx) (str px "px Veranda"))
-    (.strokeText ctx (pc bl q) 10 100)
+    (set! (.-font ctx) (str px "px Roboto"))
+    (.strokeText ctx (pc c r) x (+ px y))
+)
+
+(defn draw-tile [i j w h x y c1 c2]
+    (let [active-color (if (= (mod i 2) (mod j 2)) c1 c2)
+          t (+ y (* i w))
+          r (+ x (* j h))]
+        (set! (.-fillStyle ctx) active-color)
+        (.fillRect ctx r, t, w, h)
+    )
+)
+
+(defn draw-board []
+    (for [i (range 8)
+          j (range 8)]
+          (draw-tile i j 50 50 100 100 "rgb(255,255,255)" "rgb(0,0,0)"))
 )
