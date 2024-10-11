7e1d4af7be1e5d1709a1d52e76c39046696c202f
dcw <-->
2024-10-10 22:51:07 -0400

    refactored grid function for more awesomeness

--- chess/chessoids/src/chessoids/core.cljs
+++ chess/chessoids/src/chessoids/core.cljs
@@ -26,25 +26,32 @@
 ; (piece color rank)
 (defn pc [c r] ((pieces c) r))
 
-(defn draw-pc [c r x y px]
+(defn draw-pc [piece x y px]
     (set! (.-fillStyle ctx) "rgb(0,0,255)")
     (.fillRect ctx x, y, px, px)
     (set! (.-fillStyle ctx) "rgb(255,0,0)")
     (set! (.-font ctx) (str px "px Roboto"))
-    (.strokeText ctx (pc c r) x (+ px y))
+    (.strokeText ctx piece x (+ px y))
 )
 
-(defn draw-tile [i j w h x y c1 c2]
-    (let [active-color (if (= (mod i 2) (mod j 2)) c1 c2)
-          t (+ y (* i w))
-          r (+ x (* j h))]
-        (set! (.-fillStyle ctx) active-color)
-        (.fillRect ctx r, t, w, h)
+(defn gen-grid-coord-fn [origin-x origin-y grid-square-width grid-square-height]
+    (fn [i j] [(+ origin-x (* grid-square-width i)) (+ origin-y (* grid-square-height j))])
+)
+
+(defn gen-fill-rect-grid [grid-fn w h context]
+    (fn [i j color]
+        (let [[x y] (grid-fn i j)]
+            (set! (.-fillStyle ctx) color)
+            (.fillRect context x y w h)
+        )
     )
 )
+
+(def gcf (gen-grid-coord-fn 100 100 20 20))
+(def basic-fill-rect (gen-fill-rect-grid gcf 20 20 ctx))
 
 (defn draw-board []
     (for [i (range 8)
           j (range 8)]
-          (draw-tile i j 50 50 100 100 "rgb(255,255,255)" "rgb(0,0,0)"))
+          (basic-fill-rect i j (if (= (mod i 2) (mod j 2)) "rgb(255,255,255)" "rgb(0,0,0)")))
 )
