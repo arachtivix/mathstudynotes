cf94726324e7734a45b4d773e145ae428ccfbad5
Daniel Werner <danielcswerner@gmail.com>
2025-01-04 18:39:44 -0500

    Adding the ecr to terraform

--- chess/blender/cloud-build.tf
+++ chess/blender/cloud-build.tf
@@ -18,3 +18,22 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Get reference to existing ECR repository
+data "aws_ecr_repository" "blender" {
+  name = "wernerware-blender"
+}
+
+# Output the repository URL for reference
+output "repository_url" {
+  value = data.aws_ecr_repository.blender.repository_url
+}
+
+# Output the registry ID
+output "registry_id" {
+  value = data.aws_ecr_repository.blender.registry_id
+}
+
+# Output the ARN
+output "repository_arn" {
+  value = data.aws_ecr_repository.blender.arn
+}
