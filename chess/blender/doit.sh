d28c2def919444fe86334ba66d2e6613a17524c8
Daniel Werner <danielcswerner@gmail.com>
2025-01-05 18:33:42 -0500

    -b for e'rthing

--- chess/blender/doit.sh
+++ chess/blender/doit.sh
@@ -4,6 +4,6 @@ set -e
 rm -rf renders
 
 export PYTHONPATH=$(pwd)
-blender --debug --python-exit-code 1 --python-use-system-env --python make.py
+blender --debug -b --python-exit-code 1 --python-use-system-env --python make.py
 # render does not have to be separate, but it may help with stuff
 blender /tmp/saved.blend -b -a --threads 15
