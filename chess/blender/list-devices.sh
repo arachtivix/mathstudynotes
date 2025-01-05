667eaa3ec5364c94dabaf84a47ee598dedaec33d
Daniel Werner <danielcswerner@gmail.com>
2025-01-05 16:54:19 -0500

    background

--- chess/blender/list-devices.sh
+++ chess/blender/list-devices.sh
@@ -2,4 +2,4 @@
 set -e
 
 export PYTHONPATH=$(pwd)
-blender --debug --python-exit-code 1 --python-use-system-env --python list-devices.py
+blender --debug --background --python-exit-code 1 --python-use-system-env --python list-devices.py
