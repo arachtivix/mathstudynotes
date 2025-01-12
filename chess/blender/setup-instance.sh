c25c28c9562c1ffa6f5527945eba4704f14d4944
Daniel Werner <danielcswerner@gmail.com>
2025-01-12 12:59:15 -0500

    wqMore tweaks

--- chess/blender/setup-instance.sh
+++ chess/blender/setup-instance.sh
@@ -1,11 +1,17 @@
 systemctl enable amazon-ssm-agent
 systemctl start amazon-ssm-agent
 
+
+echo "Setting up Blender instance..."
 cd ~
 git clone https://github.com/arachtivix/mathstudynotes
 cd mathstudynotes/chess/blender
 
 sudo sh install-bender.sh
 
+echo "Installing Blender..."
+sudo sh install-bender.sh
+
+echo "Syncing assets from S3 bucket"
 mkdir -p assets
 aws s3 sync s3://wernerware-blender-assets ./assets/
