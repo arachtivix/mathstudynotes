1fc2351a3cb6d95054e3fa121877504ae8969b47
dcw <-->
2025-05-24 13:13:14 -0400

    composed some thoughts on euler p206

---
+++ project_euler/proj/src/proj/p206/core.clj
@@ -0,0 +1,16 @@
+
+(ns proj.p206.core
+  (:require [clojure.math :as math]
+            [proj.shared :as shared])
+  (:gen-class))
+
+;; Problem 206: Concealed Square
+;; Created on 2025-05-23
+
+(defn solve []
+  ;; TODO: Implement solution
+  nil)
+
+(defn -main []
+  (println "Solution to Problem 206:")
+  (println (solve)))
