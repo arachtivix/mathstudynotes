a7b33580acc73f18a715a8f1c1ec07400f12c51e
Daniel Werner <danielcswerner@gmail.com>
2025-05-26 12:09:07 -0400

    Wrote code for second approach for p206, fixed bug in solution 1

--- project_euler/proj/src/proj/p206/core.clj
+++ project_euler/proj/src/proj/p206/core.clj
@@ -20,7 +20,7 @@
     (if counts-match pairwise-all-good false)))
 
 (defn is-root-of-hidden-sq? [n] (matches-pattern?
-                        '(1 :_ 2 :_ 3 :_ 4 :_ 5 :_ 6 :_ 7 :_ 8 :_ 9 :_ 0 :_)
+                        '(1 :_ 2 :_ 3 :_ 4 :_ 5 :_ 6 :_ 7 :_ 8 :_ 9 :_ 0)
                         (dec-exp-int (* n n))))
 
 (def max-possible-hidden-square 19293949596979899909N)
@@ -30,7 +30,24 @@
 (def num-brute-force (+ 1 (- max-n min-n)))
 
 ;; naive solution will take its sweet time
-(defn sol1 [] (take 1 (filter is-root-of-hidden-sq? (range 3194219858N 4392487860N))))
+(defn sol1 [] (take 1 (filter is-root-of-hidden-sq? (range min-n max-n))))
+
+
+(def max-m-possible-hidden-square 192939495969798999N)
+(def max-m (bigint (math/sqrt max-m-possible-hidden-square)))
+(def min-m-possible-hidden-square 102030405060708090N)
+(def min-m (bigint (math/sqrt min-m-possible-hidden-square)))
+(def num-m-brute-force (+ 1 (- max-m min-m)))
+
+(defn is-m-sq? "Using the intermidiate step from the writeup"
+  [m] (matches-pattern?
+       '(1 :_ 2 :_ 3 :_ 4 :_ 5 :_ 6 :_ 7 :_ 8 :_ 9)
+       (dec-exp-int (* m m))))
+
+;; technically better, but also takes its sweet time
+(defn sol2 [] (take 1 (filter is-m-sq? (range min-m max-m))))
+
+
 
 (defn solve []
   ;; TODO: Implement solution
