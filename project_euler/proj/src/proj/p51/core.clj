9d3f7252810584d458ac208fcae4fadd2dfe1c44
dcw <-->
2025-06-09 19:06:21 -0400

    p51

---
+++ project_euler/proj/src/proj/p51/core.clj
@@ -0,0 +1,16 @@
+
+(ns proj.p51.core
+  (:require [clojure.math :as math]
+            [proj.shared :as shared])
+  (:gen-class))
+
+;; Problem 51: Prime Digit Replacements
+;; Created on 2025-06-09
+
+(defn solve []
+  ;; TODO: Implement solution
+  nil)
+
+(defn -main []
+  (println "Solution to Problem 51:")
+  (println (solve)))
