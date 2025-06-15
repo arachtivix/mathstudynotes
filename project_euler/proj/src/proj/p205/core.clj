2f96300d520dc0947036b9a8caf35ce7f1d9e708
Daniel Werner <danielcswerner@gmail.com>
2025-06-15 14:38:01 -0400

    p205 wip

---
+++ project_euler/proj/src/proj/p205/core.clj
@@ -0,0 +1,16 @@
+
+(ns proj.p205.core
+  (:require [clojure.math :as math]
+            [proj.shared :as shared])
+  (:gen-class))
+
+;; Problem 205: Dice Game
+;; Created on 2025-06-15
+
+(defn solve []
+  ;; TODO: Implement solution
+  nil)
+
+(defn -main []
+  (println "Solution to Problem 205:")
+  (println (solve)))
