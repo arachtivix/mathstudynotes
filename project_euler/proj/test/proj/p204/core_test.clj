346a126550df2f68c223868b799abf0d7f6aeeee
Daniel Werner <danielcswerner@gmail.com>
2025-06-21 11:31:27 -0400

    adding some more test scenarios for the simple

--- project_euler/proj/test/proj/p204/core_test.clj
+++ project_euler/proj/test/proj/p204/core_test.clj
@@ -13,10 +13,13 @@
 
 (deftest simple-generalized-hamming-numbers-impl-seq-test
   (testing "Testing simple-generalized-hamming-numbers-impl-seq"
-    (is (= '(1 2 3 4 5 6 8 9 10 12 15) 
-           (take 11 (simple-generalized-hamming-numbers-impl-seq 5))))))
+    (is (= '(1 2 3 4 5 6 8 9 10 12 15)
+           (take 11 (simple-generalized-hamming-numbers-impl-seq 5))))
+    (is (= '(1 2 3 4 5 6 7 8 9 10 12 14 15 16 18 20)
+           (take 16 (simple-generalized-hamming-numbers-impl-seq 7))))))
 
 (deftest brute-force-count-test
   (testing "Testing brute-force-count"
     (is (= 11 (brute-force-count 5 15)))
-    (is (= 4 (brute-force-count 5 4)))))
+    (is (= 4 (brute-force-count 5 4)))
+    (is (= 17 (brute-force-count 7 23)))))
