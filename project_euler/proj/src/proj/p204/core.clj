3feebf1bc6f41f00ef2aa836481f767400650d46
dcw <-->
2025-06-20 20:18:26 -0400

    Starting p204

---
+++ project_euler/proj/src/proj/p204/core.clj
@@ -0,0 +1,16 @@
+
+(ns proj.p204.core
+  (:require [clojure.math :as math]
+            [proj.shared :as shared])
+  (:gen-class))
+
+;; Problem 204: Generalised Hamming Numbers
+;; Created on 2025-06-20
+
+(defn solve []
+  ;; TODO: Implement solution
+  nil)
+
+(defn -main []
+  (println "Solution to Problem 204:")
+  (println (solve)))
--- project_euler/proj/src/proj/p205/core.clj
+++ project_euler/proj/src/proj/p205/core.clj
@@ -1,7 +1,5 @@
 
 (ns proj.p205.core
-  (:require [clojure.math :as math]
-            [proj.shared :as shared])
   (:gen-class))
 
 ;; Problem 205: Dice Game
