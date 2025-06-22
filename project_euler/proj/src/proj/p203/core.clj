da201f3dca5e7f11fcff38417c0bfe8b09bb187d
dcw <-->
2025-06-22 15:26:38 -0400

    added hopefully useful tool for p203

--- project_euler/proj/src/proj/p203/core.clj
+++ project_euler/proj/src/proj/p203/core.clj
@@ -7,6 +7,9 @@
 ;; Problem 203: Squarefree Binomial Coefficients
 ;; Created on 2025-06-22
 
+(defn next-pascal-row [p-row-prev]
+  (cons 1 (conj (into [] (map #(+ %1 %2) p-row-prev (rest p-row-prev))) 1)))
+
 (defn solve []
   ;; TODO: Implement solution
   nil)
--- project_euler/proj/test/proj/p203/core_test.clj
+++ project_euler/proj/test/proj/p203/core_test.clj
@@ -3,6 +3,13 @@
   (:require [clojure.test :refer [deftest is testing]]
             [proj.p203.core :refer :all]))
 
+(deftest next-pascal-row-test
+  (testing "Testing next-pascal-row"
+    (is (= [1 1] (next-pascal-row [1])))
+    (is (= [1 2 1] (next-pascal-row [1 1])))
+    (is (= [1 3 3 1] (next-pascal-row [1 2 1])))
+    (is (= [1 4 6 4 1] (next-pascal-row [1 3 3 1])))))
+
 (deftest solve-test
   (testing "Testing solution"
     ;; TODO: Add test cases
