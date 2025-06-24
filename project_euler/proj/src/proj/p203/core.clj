3e7cc433cb8d45ab487a0cf5c2e9242ca4e17e9b
dcw <-->
2025-06-23 21:40:41 -0400

    p203 done

--- project_euler/proj/src/proj/p203/core.clj
+++ project_euler/proj/src/proj/p203/core.clj
@@ -10,7 +10,7 @@
 (defn next-pascal-row [p-row-prev]
   (cons 1 (conj (into [] (map #(+ %1 %2) p-row-prev (rest p-row-prev))) 1)))
 
-(def pascal-seq
+(def pascal-seq ; turns out I probably did not need to do this
   (map #((% 1) (% 0))
        (iterate
         #(let [idx (% 0)
@@ -21,9 +21,23 @@
              [nxt nbrs]))
         [0 [1]])))
 
+(defn take-rows [rows]
+  (let [el-count (quot (+ (* rows rows) rows) 2)]
+    (take el-count pascal-seq)))
+
+(defn has-square-factor 
+  ([n] (has-square-factor n 2))
+  ([n test] (let [test-sq (* test test)]
+              (cond (> test-sq n) false
+                    (= (mod n test-sq) 0) true
+                    :else (recur n (+ test 1))))))
+
+(defn solve-1 [rows]
+    (reduce + (filter #(not (has-square-factor %)) (distinct (take-rows rows)))))
+
 (defn solve []
   ;; TODO: Implement solution
-  nil)
+  (solve-1 50))
 
 (defn -main []
   (println "Solution to Problem 203:")
--- project_euler/proj/test/proj/p203/core_test.clj
+++ project_euler/proj/test/proj/p203/core_test.clj
@@ -14,7 +14,14 @@
   (testing "Testing pascal-seq"
     (is (= '(1 1 1 1 2 1 1 3 3 1 1 4 6 4) (take 14 pascal-seq)))))
 
-(deftest solve-test
-  (testing "Testing solution"
-    ;; TODO: Add test cases
-    (is (= nil (solve)))))
+(deftest has-square-factor-test
+  (testing "Testing has-square-factor"
+    (is (= true (has-square-factor 4)))
+    (is (= true (has-square-factor 75)))
+    (is (= false (has-square-factor 5)))
+    (is (= false (has-square-factor 13)))
+    (is (= false (has-square-factor (* 13 7))))))
+
+(deftest test-sol-1
+  (testing "Testing solve-1"
+    (is (= 105 (solve-1 8)))))
